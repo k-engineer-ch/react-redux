b39f948b9f28235e6b2960ef776678b6ed434489
k-engineer-ch <k.engineer.ch2023@gmail.com>
2024-02-04 10:29:57 +0900

    5-3 useState を Redux で置き換える - Card の削

--- src/reducer.ts
+++ src/reducer.ts
@@ -1,6 +1,6 @@
 import { Reducer } from 'redux'
 import produce from 'immer'
-import { sortBy } from './util'
+import { sortBy, reorderPatch } from './util'
 import { CardID, ColumnID } from './api'
 
 export type State = {
@@ -15,6 +15,7 @@ export type State = {
     }[]
   }[]
   cardsOrder: Record<string, CardID | ColumnID | null>
+  deletingCardID?: CardID
 }
 
 const initialState: State = {
@@ -47,7 +48,20 @@ export type Action =
           text?: string
         }[]
         cardsOrder: Record<string, CardID | ColumnID | null>
+        deletingCardID?: CardID
       }
+    }
+  | {
+      type: 'Card.SetDeletingCard'
+      payload: {
+        cardID: CardID
+      }
+    }
+  | {
+      type: 'Dialog.ConfirmDelete'
+    }
+  | {
+      type: 'Dialog.CancelDelete'
     }
 
 export const reducer: Reducer<State, Action> = produce(
@@ -74,6 +88,39 @@ export const reducer: Reducer<State, Action> = produce(
         draft.columns?.forEach(column => {
           column.cards = sortBy(unorderedCards, cardsOrder, column.id)
         })
+        return
+      }
+
+      case 'Card.SetDeletingCard': {
+        const { cardID } = action.payload
+
+        draft.deletingCardID = cardID
+        return
+      }
+
+      case 'Dialog.ConfirmDelete': {
+        const cardID = draft.deletingCardID
+        if (!cardID) return
+
+        draft.deletingCardID = undefined
+
+        const column = draft.columns?.find(col =>
+          col.cards?.some(c => c.id === cardID),
+        )
+        if (!column?.cards) return
+
+        column.cards = column.cards.filter(c => c.id !== cardID)
+
+        const patch = reorderPatch(draft.cardsOrder, cardID)
+        draft.cardsOrder = {
+          ...draft.cardsOrder,
+          ...patch,
+        }
+        return
+      }
+
+      case 'Dialog.CancelDelete': {
+        draft.deletingCardID = undefined
         return
       }
 
